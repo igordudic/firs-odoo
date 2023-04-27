ab54fcb579a5fc1d2b8f1c2d65d18fc580569592
Akhil <cybroodoo@gmail.com>
2023-04-27 16:37:42 +0530

    [update]

--- 14.1/firs_report/models/account_tax.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models,fields
-
-class AccountTax(models.Model):
-    _inherit = 'account.tax'
-    
-    tax_type = fields.Selection([('Vat','Vat'),('Consumption','Consumption')],string='Tax Type')
